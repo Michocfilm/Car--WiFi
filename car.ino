9679d77a2768a562c75617173c882fe0955fbcb1
Michocfilm <104713168+Michocfilm@users.noreply.github.com>
2024-10-12 11:28:45 +0700

    first commit

---
+++ car.ino
@@ -0,0 +1,121 @@
+
+#define BLYNK_TEMPLATE_ID "TMPLvnFQPOAa"
+#define BLYNK_TEMPLATE_NAME "car"
+#define BLYNK_FIRMWARE_VERSION        "0.1.0"
+#define BLYNK_PRINT Serial
+#define APP_DEBUG
+#include "BlynkEdgent.h"
+#define BLYNK_PRINT Serial
+#include <ESP8266WiFi.h>
+#define ENA D0
+#define IN1 D1
+#define IN2 D2
+#define IN3 D3
+#define IN4 D4
+#define ENB D5
+#include <Servo.h>
+#define servo1 D6
+Servo servo;
+
+bool forward = 0;
+bool backward = 0;
+bool left = 0;
+bool right = 0;
+int Speed;
+
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(ENA, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(ENB, OUTPUT);
+  servo.attach(servo1); // NodeMCU D4 pin
+  BlynkEdgent.begin();
+}
+BLYNK_WRITE(V0) {
+  forward = param.asInt();
+}
+ 
+BLYNK_WRITE(V1) {
+  backward = param.asInt();
+}
+ 
+BLYNK_WRITE(V2) {
+  left = param.asInt();
+}
+ 
+BLYNK_WRITE(V3) {
+  right = param.asInt();
+}
+ 
+BLYNK_WRITE(V4) {
+  Speed = param.asInt();
+}
+
+BLYNK_WRITE(V5)
+{
+   servo.write(param.asInt());
+}
+void smartcar() {
+  if (forward == 1) {
+    carforward();
+    Serial.println("carforward");
+  } else if (backward == 1) {
+    carbackward();
+    Serial.println("carbackward");
+  } else if (left == 1) {
+    carturnleft();
+    Serial.println("carfleft");
+  } else if (right == 1) {
+    carturnright();
+    Serial.println("carright");
+  } else if (forward == 0 && backward == 0 && left == 0 && right == 0) {
+    carStop();
+    Serial.println("carstop");
+  }
+}
+void loop() {
+  BlynkEdgent.run();
+  smartcar();
+}
+void carforward() {
+  analogWrite(ENA, Speed);
+  analogWrite(ENB, Speed);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+void carbackward() {
+  analogWrite(ENA, Speed);
+  analogWrite(ENB, Speed);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
+void carturnleft() {
+  analogWrite(ENA, Speed);
+  analogWrite(ENB, Speed);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+void carturnright() {
+  analogWrite(ENA, Speed);
+  analogWrite(ENB, Speed);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
+void carStop() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+}
